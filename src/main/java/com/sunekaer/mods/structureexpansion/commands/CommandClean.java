6617e7aba726485c0ed6ea6695bed98e6e7ff38a
Sunekaer <git@sunekaer.com>
2022-06-12 00:51:33 +0200

    1.19 port

--- src/main/java/com/sunekaer/mods/structureexpansion/commands/CommandClean.java
+++ src/main/java/com/sunekaer/mods/structureexpansion/commands/CommandClean.java
@@ -4,12 +4,12 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.levelgen.structure.templatesystem.StructureManager;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 
 public class CommandClean {
 	public static ArgumentBuilder<CommandSourceStack, ?> register() {
@@ -22,7 +22,7 @@ public class CommandClean {
 
 	private static int cleanStruc(CommandSourceStack source, String file) {
 		ServerLevel worldServer = source.getLevel();
-		StructureManager templateManager = worldServer.getStructureManager();
+		StructureTemplateManager templateManager = worldServer.getStructureManager();
 		ResourceLocation name = new ResourceLocation(file);
 		StructureTemplate template = templateManager.getOrCreate(name);
 
@@ -30,7 +30,7 @@ public class CommandClean {
 			int preSize = e.blocks().size();
 			e.blocks().removeIf(a -> a.state.getBlock() == Blocks.AIR);
 			int removed = preSize - e.blocks().size();
-			source.sendSuccess(new TranslatableComponent("Removed " + removed + " air blocks"), true);
+			source.sendSuccess(Component.literal("Removed " + removed + " air blocks"), true);
 		});
 
 		templateManager.save(name);
